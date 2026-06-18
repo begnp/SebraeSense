e7c010842fafd9936af89a5adad4b9808af0babb
cassianvasc <nbcassferrari@gmail.com>
2026-06-18 14:22:06 -0300

    alteraçao no database/backend para deploy

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://sense_user:sense_password@localhost/sense_db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://sense_user:sense_password@localhost/sense_db"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
